87555118c3ce9f36cf637f2743d0cc3a55ccfb46
RemiIO <i@linux.dog>
2019-06-26 13:33:51 +0800

    add fullpage

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,41 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <full-page ref="fullpage" :options="options" id="fullpage">
+      <div class="section">
+        <h2>Second 1</h2>
+      </div>
+      <div class="section">
+        <h2>Second 2</h2>
+      </div>
+      <div class="section">
+        <h2>Second 3</h2>
+      </div>
+    </full-page>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  data() {
+    return {
+      options: {
+        menu: "#menu",
+        anchors: ["page1", "page2", "page3"],
+        sectionsColor: ["#41b883", "#ff5f45", "#0798ec"]
+      }
+    };
   }
-}
+};
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+}
+.section{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: aliceblue;
 }
 </style>
